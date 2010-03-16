382450343cb6677eecdab165c5f3b39b7f1e55be
tremst <tremst@a0af6c34-c4a6-11de-896c-518660fad6c5>
2010-03-16 14:46:12 +0000

    * alcune correzioni

--- trunk/main.py
+++ trunk/main.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-questo file contiene il main; ho aggiunto qualche funzione per misurare 
+questo file contiene il main; ho aggiunto qualche funzione per misurare
 il tempo di esecuzione.
 il primo algoritmo è quello originale.
 le definizioni degli algoritmi si trovano nel file sequentialEliminationAlgorithm.py
-NB: probabilmente all'inizio dell'esecuzione vedrà alcuni warning; riguardano 
+NB: probabilmente all'inizio dell'esecuzione vedrà alcuni warning; riguardano
 una libreria usata non l'applicazione.
 """
 
@@ -28,7 +28,7 @@ def get_edges_from_file(filename):
         file = open(filename, 'r')
         def get_edge(list):
             return list[1], list[2]
-        edges = [get_edge(line.split()) for line in file 
+        edges = [get_edge(line.split()) for line in file
                  if line.find('e', 0, 1) >= 0]
         return edges
     except:
@@ -63,26 +63,28 @@ def main(args):
 #    filename2 = cwd + "/benchmarks/" + "brock200_1" + ext
 #    filenames = [filename1, filename2]
 #    filename = cwd + "/benchmarks/" + testname + ext
-    LOG_FILENAME = './result.log'
+    LOG_FILENAME = './result_cardinality.log'
     logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
     print "*****************************"
-    for ignore, ignore, files in os.walk("./test/"):
-        for file in files:
-            filename = "./test/" + file
-            graph = Graph(get_edges_from_file(filename))
-            text = ''.join(["graph: ", file, ", nodes: ", str(len(graph)), ", edges: ", 
-                            str(graph.number_of_edges())])
-            logging.info(text)
-            print text
-#            upper_bound_fun = UBA.upper_bound_from_linear_coloring
-#            sequential_elimination_algorithm_1(graph, upper_bound_fun)
-#            print "--"
-#            graph = Graph(get_edges_from_file(filename))
-            upper_bound_fun_1 = UBA.upper_bound_from_cardinality
-            upper_bound_fun_2 = UBA.upper_bound_from_cardinality
-            sequential_elimination_algorithm_2(graph, upper_bound_fun_1, upper_bound_fun_2)
-            logging.info('---')
-            print "----"
+    for dirpath, ignore, files in os.walk("./test/"):
+        if dirpath == "./test/":
+            for nfile in files:
+                if nfile != ".directory":
+                    filename = os.path.join(dirpath, nfile)
+                    graph = Graph(get_edges_from_file(filename))
+                    text = ''.join(["graph: ", str(nfile), ", nodes: ", str(len(graph)), ", edges: ",
+                                    str(graph.number_of_edges())])
+                    logging.info(text)
+                    print text
+        #            upper_bound_fun = UBA.upper_bound_from_cardinality
+        #            sequential_elimination_algorithm_1(graph, upper_bound_fun)
+        #            print "--"
+        #            graph = Graph(get_edges_from_file(filename))
+                    upper_bound_fun_1 = UBA.upper_bound_from_cardinality
+                    upper_bound_fun_2 = UBA.upper_bound_from_cardinality
+                    sequential_elimination_algorithm_2(graph, upper_bound_fun_1, upper_bound_fun_2)
+                    logging.info('---')
+                    print "----"
 
 if __name__ == "__main__":
     main(sys.argv)
