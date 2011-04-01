e07446a1266f7618ccc11ee2d7d056de27a84580
tremst <tremst@a0af6c34-c4a6-11de-896c-518660fad6c5>
2011-04-01 21:56:56 +0000

    * ottimizzazioni agli algoritmi per il calcolodegli upperbounds

--- trunk/main.py
+++ trunk/main.py
@@ -16,11 +16,10 @@ import psyco
 from src.graphStructure import Graph, get_edges_from_file
 from src.sequentialEliminationAlgorithm import \
                                         sequential_elimination_algorithm_1, \
-                                        sequential_elimination_algorithm_1_p, \
-                                        sequential_elimination_algorithm_2, \
-                                        sequential_elimination_algorithm_2_p
+                                        sequential_elimination_algorithm_2
 import src.upperBoundAlgorithms as UBA
 import logging
+from timeit import Timer
 
 """
 main
@@ -28,16 +27,16 @@ main
 def main(args):
     psyco.full()
     logging.basicConfig(filename='./results.log', level=logging.INFO, format="%(message)s")
-#    fun_1 = [UBA.upper_bound_from_cardinality_1, UBA.upper_bound_from_linear_coloring_1,
-#                UBA.upper_bound_from_dsatur_1]
-    fun_1 = [UBA.upper_bound_from_cardinality_1_p]    
-#    fun_2 = [UBA.upper_bound_from_cardinality_2, UBA.upper_bound_from_linear_coloring_2,
-#                UBA.upper_bound_from_dsatur_2]
-    fun_2 = [UBA.upper_bound_from_cardinality_2_p]    
     log = ['cardinality', 'linear coloring', 'dsatur']
+    fun_1 = [UBA.upper_bound_from_cardinality_1, \
+             UBA.upper_bound_from_linear_coloring_1]#, \
+             #UBA.upper_bound_from_dsatur_1]
+    fun_2 = [UBA.upper_bound_from_cardinality_2, \
+             UBA.upper_bound_from_linear_coloring_2]#, \
+             #UBA.upper_bound_from_dsatur_2]
     for dirpath, ignore, files in os.walk("./test/"):
         if dirpath == "./test/":
-            for i in range(1):
+            for i in range(2):
                 text = "---------- " + log[i]
                 logging.info(text)
                 print text
@@ -45,73 +44,45 @@ def main(args):
                 for nfile in files:
                     if nfile != ".directory":
                         filename = os.path.join(dirpath, nfile)
-#                        graph = Graph(get_edges_from_file(filename))
-#                        upper_bound_fun_1 = fun_2[i]
-#                        sequential_elimination_algorithm_1_p(nfile, graph, upper_bound_fun_1)
-                        graph = Graph(get_edges_from_file(filename))
-                        upper_bound_fun_1 = fun_1[i]
-                        upper_bound_fun_2 = fun_2[i]
-                        sequential_elimination_algorithm_2_p(nfile, graph, 
-                                                             upper_bound_fun_1, 
-                                                             upper_bound_fun_2)
-#    fun_1 = [UBA.upper_bound_from_cardinality_1] 
-#    fun_2 = [UBA.upper_bound_from_cardinality_2]
-#    for dirpath, ignore, files in os.walk("./test/"):
-#        if dirpath == "./test/":     
-#            for i in range(1):
-#                text = "---------- " + log[i]
-#                logging.info(text)
-#                print text
-#                files.sort()
-#                for nfile in files:
-#                    if nfile != ".directory":
-#                        filename = os.path.join(dirpath, nfile)
-#                        graph = Graph(get_edges_from_file(filename))
-#                        upper_bound_fun_1 = fun_2[i]
-#                        sequential_elimination_algorithm_1(nfile, graph, upper_bound_fun_1)
-#                        graph = Graph(get_edges_from_file(filename))
-#                        upper_bound_fun_1 = fun_1[i]
-#                        upper_bound_fun_2 = fun_2[i]
-#                        sequential_elimination_algorithm_2(nfile, graph, 
-#                                                             upper_bound_fun_1, 
-#                                                             upper_bound_fun_2)                        
-#    logging.info("********************************************")
-#    print "********************************************"
-#    fun_1 = [UBA.upper_bound_from_cardinality, UBA.upper_bound_from_linear_coloring,
-#             UBA.upper_bound_from_dsatur]
-#    for dirpath, ignore, files in os.walk("./test/"):
-#        if dirpath == "./test/":
-#            for i in range(3):
-#                text = "---------- " + log[i]
-#                logging.info(text)
-#                print text
-#                files.sort()
-#                for nfile in files:
-#                    if nfile != ".directory":
-#                        filename = os.path.join(dirpath, nfile)
-#                        graph = Graph(get_edges_from_file(filename))
-#                        upper_bound_fun_1 = fun_1[i]
-#                        upper_bound_fun_1(nfile, graph)
-    #fun_1 = [UBA.upper_bound_from_cardinality_1]
-    #fun_2 = [UBA.upper_bound_from_linear_coloring_2,
-             #UBA.upper_bound_from_dsatur_2]
-    #log = ['linear coloring', 'dsatur']
+                        graph = Graph()
+                        graph.add_edges(get_edges_from_file(filename))
+                        upper_bound_fun_1 = fun_2[i]
+                        sequential_elimination_algorithm_1(nfile, graph, upper_bound_fun_1)
+                        #graph = Graph(get_edges_from_file(filename))
+                        #upper_bound_fun_1 = fun_1[i]
+                        #upper_bound_fun_2 = fun_2[i]
+                        #sequential_elimination_algorithm_2(nfile, graph,
+                                                             #upper_bound_fun_1,
+                                                             #upper_bound_fun_2)
+                        #graph = Graph()
+                        #graph.add_edges(get_edges_from_file(filename))
+                        #upper_bound_fun_1 = fun_1[i]
+                        #upper_bound_fun_2 = fun_2[i]
+                        #sequential_elimination_algorithm_2_p(nfile, graph,
+                                                             #upper_bound_fun_1,
+                                                             #upper_bound_fun_2)
+    logging.info("********************************************")
+    print "********************************************"
+    #fun_1 = [UBA.upper_bound_from_cardinality_, UBA.upper_bound_from_cardinality]
     #for dirpath, ignore, files in os.walk("./test/"):
         #if dirpath == "./test/":
-            #for i in range(2):
-                #text = "---------- " + log[i]
-                #logging.info(text)
-                #print text
+            #for i in range(10):
+                ##text = "---------- " + log[i]
+                ##logging.info(text)
+                ##print text
                 #files.sort()
                 #for nfile in files:
                     #if nfile != ".directory":
                         #filename = os.path.join(dirpath, nfile)
-                        #graph = Graph(get_edges_from_file(filename))
-                        #upper_bound_fun_1 = fun_1[0]
-                        #upper_bound_fun_2 = fun_2[i]
-                        #sequential_elimination_algorithm_2(nfile, graph,
-                                                           #upper_bound_fun_1,
-                                                           #upper_bound_fun_2)
+                        ##graph = Graph(get_edges_from_file(filename))
+                        ##upper_bound_fun_1 = fun_1[0]
+                        ##upper_bound_fun_1(nfile, graph)
+                        #graph = Graph()
+                        #graph.add_edges(get_edges_from_file(filename))
+                        ##graph = Graph(get_edges_from_file(filename))
+                        #upper_bound_fun_1 = fun_1[1]
+                        #upper_bound_fun_1(nfile, graph)
+                        #upper_bound_fun_1(nfile, graph)
 
 if __name__ == "__main__":
     main(sys.argv)
